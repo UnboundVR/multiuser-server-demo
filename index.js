e4e42afb12ad1c949214340462d64358bea0d702
Guido Leandro Maliandi <guido.lm@gmail.com>
2016-11-20 20:46:52 -0300

    adding syncing of position/rotation via a state property

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import http from 'http';
 import socketio from 'socket.io';
 import syncOnlineUsers from './lib/syncOnlineUsers';
+import syncPosition from './lib/syncPosition';
+import UserStore from './lib/userStore';
 
 let app = express();
 let server = http.createServer(app);
@@ -12,4 +14,6 @@ server.listen(port, () => {
   console.log(`Listening at port ${port}`);
 });
 
-syncOnlineUsers(io);
+let store = new UserStore();
+syncOnlineUsers(io, store);
+syncPosition(io, store);
